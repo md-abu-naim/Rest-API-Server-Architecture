9d620778167e7e6d24587bc6605441c76c10c8aa
abunaim34 <farhanadnanfarabi@gmail.com>
2026-05-12 20:33:12 +0600

    server create

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,22 @@
-import { createServer, IncomingMessage, Server } from "node:http";
+import { createServer, IncomingMessage, ServerResponse, type Server } from "http";
 
 
-const server: Server = createServer((req: IncomingMessage, res) => {
-    console.log(req);
+const server: Server = createServer((req: IncomingMessage, res: ServerResponse) => {
+    console.log('Server runing');
+    const url = req.url
+    const method = req.method
+
+    if (url === '/' && method === "GET") {
+        res.writeHead(200, { "content-type": "text/json" })
+        res.end(JSON.stringify({ message: 'This is root ' }))
+    } else if (url?.startsWith('/products')) {
+        res.writeHead(200, { "content-type": "text/json" })
+        res.end(JSON.stringify({ message: 'This is produts route ' }))
+    } else {
+
+        res.writeHead(404, { "content-type": "text/josn" })
+        res.end(JSON.stringify({ message: 'page not found' }))
+    }
 })
 
 server.listen(5000, () => {
